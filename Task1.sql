c7a32483f866b5008a9b2f3f30818e7842a77194
noor7599 <126394883+noor7599@users.noreply.github.com>
2023-02-27 09:17:25 -0800

    Add files via upload

---
+++ Task1.sql
@@ -0,0 +1,23 @@
+SELECT*
+FROM customers
+WHERE creditLimit >= 1000000 AND customerNumber< 200 OR country = 'USA';
+
+
+
+
+SELECT*
+FROM customers
+WHERE (creditLimit + 2000)
+ORDER BY creditLimit+ 2000  DESC
+
+
+ AS new_credit
+  new_credit
+  
+  SELECT*
+  FROM customers
+  ORDER BY creditLimit DESC
+  LIMIT 3;
+
+
+
---
+++ Task2.sql
@@ -0,0 +1,16 @@
+SELECT*
+  FROM customers
+  WHERE contactFirstName regexp 'el' or 'ie';
+
+SELECT*
+FROM customers
+WHERE contactFirstName regexp 'je' or 'le';
+
+
+SELECT*
+FROM customers
+WHERE contactFirstName regexp 'bg | ba'
+
+
+
+
